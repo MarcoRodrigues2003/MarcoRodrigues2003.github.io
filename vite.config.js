a555ae3328dbce2070e3138eb0cf7af017a1a55e
Marco Rodrigues <122059113+MarcoRodrigues2003@users.noreply.github.com>
2026-01-09 16:11:56 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === "production" ? "/gabriel-portfolio/" : "/",
+  base: '/'
 }));
 
