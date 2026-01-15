455f6cdf3b5b2672b51cd2989786e382b8cdf76f
Marco Rodrigues <122059113+MarcoRodrigues2003@users.noreply.github.com>
2026-01-15 20:49:37 +0000

    Update data.js

--- src/data.js
+++ src/data.js
@@ -14,7 +14,7 @@ export const projects = [
     platform: "Mobile",
     tech: ["Unity", "C#", "Mobile"],
     short: "Hot-potato style party game with motion detection and minigames.",
-    thumb: "media/toss-the-minder.jpg",
+    thumb: "media/MinderLogo.jpg",
 
     whatIs:
       "Toss The Minder is a party game with multiple minigames where players pass the plush around while music plays.",
